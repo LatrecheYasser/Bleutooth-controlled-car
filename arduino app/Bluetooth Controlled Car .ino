dcb05907a94e08d27b4540ec88707b02d6ae2108
yasser Latreche <yasserlat@gmail.com>
2019-06-15 11:30:29 +0100

    final Ino code

---
+++ arduino app/Bluetooth Controlled Car .ino
@@ -0,0 +1,260 @@
+
+int BluetoothData; // the data given from Computer
+int Run = 0;
+int Goingup = -1;
+int Auto = 0;
+
+// MOTO MAMA pins
+
+int InA1 = 8;
+
+int InA2 = 9;
+
+int EnableA = 10;
+
+int EnableB = 11;
+
+int InB1 = 12;
+
+int InB2 = 13;
+
+// Ultra sensor Pin
+
+const int Trig = 6;
+const int Echo = 7;
+
+
+
+void setup() {
+
+
+// put your setup code here, to run once:
+
+// Inicializa los pines del motor como salidas
+
+pinMode(EnableA, OUTPUT); // sets the pin as output
+
+pinMode(InA1, OUTPUT); // sets the pin as output
+
+pinMode(InA2, OUTPUT); // sets the pin as output
+
+pinMode(EnableB, OUTPUT); // sets the pin as output
+
+pinMode(InB1, OUTPUT); // sets the pin as output
+
+pinMode(InB2, OUTPUT); // sets the pin as output
+
+digitalWrite(EnableA, LOW);
+
+digitalWrite(InA1, LOW);
+
+digitalWrite(InA2, LOW);
+
+digitalWrite(EnableB, LOW);
+
+digitalWrite(InB1, LOW);
+
+digitalWrite(InB2, LOW);
+ 
+Serial.begin(115200);
+Serial.println("Bluetooth on");
+
+}
+
+void loop() {
+
+//put your main code here, to run repeatedly:
+
+if(Serial.available() > 0){ // Checks whether data is comming from the serial port
+ BluetoothData = Serial.read(); // Reads the data from the serial port
+
+ Serial.println(BluetoothData);
+
+if(BluetoothData == 1) Run = 1;
+
+else if (BluetoothData == 0){
+Run = 0;
+digitalWrite(EnableA, LOW);
+
+digitalWrite(InA1, LOW);
+
+digitalWrite(InA2, LOW);
+
+digitalWrite(EnableB, LOW);
+
+digitalWrite(InB1, LOW);
+
+digitalWrite(InB2, LOW);
+
+}
+
+if (BluetoothData == 7) {
+  Auto = 1; 
+
+}
+else if (BluetoothData == 8){
+  Auto = 0;
+  
+}
+
+if(Run == 1){
+if (Auto == 0){
+   //if number 3 presssed it means go UP ..
+if(BluetoothData == 3){
+  Serial.println("go up");
+Up();
+Goingup = 1;
+}
+
+ // if the left button pressed
+if(BluetoothData == 4) Left(Goingup);
+
+  // if the right button pressed
+if(BluetoothData == 5) Right(Goingup);
+
+  // if the Down button pressed
+if(BluetoothData == 6) {
+  Serial.println("go down");
+  Down();
+  Goingup = 0;
+}
+}else if (Auto == 1 ){
+RunAuto();
+}
+
+}
+}
+}
+
+
+void Up(){
+  
+  digitalWrite(EnableA, HIGH);
+  
+  digitalWrite(InA1, HIGH);
+  
+  digitalWrite(InA2, LOW);
+  
+  digitalWrite(EnableB, HIGH);
+  
+  digitalWrite(InB1, HIGH);
+  
+  digitalWrite(InB2, LOW);
+}
+
+void Right(int where){
+  if(where == 1){ // is UP
+  digitalWrite(EnableA, HIGH);
+  
+  digitalWrite(InA1, LOW);
+  
+  digitalWrite(InA2, LOW);
+  
+  digitalWrite(EnableB, HIGH);
+  
+  digitalWrite(InB1, HIGH);
+  
+  digitalWrite(InB2, LOW);
+}else{ //is Down 
+  digitalWrite(EnableA, HIGH);
+  
+  digitalWrite(InA1, LOW);
+  
+  digitalWrite(InA2, LOW);
+  
+  digitalWrite(EnableB, HIGH);
+  
+  digitalWrite(InB1, LOW);
+  
+  digitalWrite(InB2, HIGH);
+
+}
+
+
+}
+
+void Left(int where){
+  if(where == 1){//going up
+  digitalWrite(EnableB, HIGH);
+  
+  digitalWrite(InA1, HIGH);
+  
+  digitalWrite(InA2, LOW);
+  
+  digitalWrite(EnableB, HIGH);
+  
+  digitalWrite(InB1, LOW);
+  
+  digitalWrite(InB2, LOW);
+}else { // going down
+  digitalWrite(EnableB, HIGH);
+  
+  digitalWrite(InA1, LOW);
+  
+  digitalWrite(InA2, HIGH);
+  
+  digitalWrite(EnableB, HIGH);
+  
+  digitalWrite(InB1, LOW);
+  
+  digitalWrite(InB2, LOW);
+}
+   
+}
+  
+void Down(){
+   digitalWrite(EnableA, HIGH);
+  
+  digitalWrite(InA1, LOW);
+  
+  digitalWrite(InA2, HIGH);
+  
+  digitalWrite(EnableB, HIGH);
+  
+  digitalWrite(InB1, LOW);
+  
+  digitalWrite(InB2, HIGH);
+}
+
+int sensor(){
+
+long duration;
+int distance;
+  
+digitalWrite(Trig, LOW);
+delayMicroseconds(2);
+
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(Trig, HIGH);
+delayMicroseconds(10);
+digitalWrite(Trig, LOW);
+
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(Echo, HIGH);
+
+// the speed of the sound is 0.034 cm/us so to calculate the distance to the object we maltuplay the duration by 0.034 divided by 2 because the sound travles 2 times this distance
+distance= duration*0.034/2;
+return distance;
+}
+
+int RunAuto(){
+  long r = random(0,2); 
+  Up();
+  int distance = sensor();
+  if(distance < 15){
+
+    if(r == 0) { 
+    Left(1);
+    Serial.println("Auto left");
+    } 
+    else {
+      Right(1);
+    Serial.println("Auto Right");  
+  }
+    
+   delay(1000);
+   Up();
+
+  }
+}
+
